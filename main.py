e83e26646fd7043b692e41dc2dec71e5feea3a7f
SHIBINSHA <shibin24888@gmail.com>
2023-10-11 21:01:27 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,81 @@
+from tkinter import *
+import pandas
+import random
+BACKGROUND_COLOR = "#B1DDC6"
+
+# Create the main window
+root = Tk()
+root.title("Flashy")
+root.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+# Load and display the images
+imagef = PhotoImage(file="images/card_front.png")
+imageb = PhotoImage(file="images/card_back.png")
+
+
+try:
+    data = pandas.read_csv("data.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("french.csv")
+    # print(original_data)
+    original_data.columns = original_data.columns.str.strip()
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+# print(to_learn)
+def next_card():
+    global current_card,flip_timer
+    root.after_cancel(flip_timer)
+    current_card= random.choice(to_learn)
+    
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+    canvas.itemconfig(card_background, image=card_back_img)
+
+flip_timer = root.after(3000, func=flip_card)
+
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+
+def next_card():
+    global current_card, flip_timer
+    root.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = root.after(3000, func=flip_card)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data.csv", index=False)
+    next_card()
+
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image,relief="flat",bg=BACKGROUND_COLOR, highlightthickness=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image,relief="flat",bg=BACKGROUND_COLOR, highlightthickness=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+next_card()
+root.mainloop()
+
+
+
